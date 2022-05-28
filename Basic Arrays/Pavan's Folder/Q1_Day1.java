4f85bb5a36b9c3787c4f8b467fca8c5107500dbd
Pavan Kamthane <95903010+Pavan-Kamthane@users.noreply.github.com>
2022-05-28 22:12:08 +0530

    Q1 Day1 Done

--- Basic Arrays/Pavan's Folder/Q1_Day1.java
+++ Basic Arrays/Pavan's Folder/Q1_Day1.java
@@ -1,2 +1,15 @@
 // question link:https://leetcode.com/problems/concatenation-of-array/
 
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int[] ans = new int[2*(nums.length)];
+        int i=0;
+        while(i<nums.length){
+            ans[i]=nums[i];
+            ans[i+nums.length]=nums[i];
+            i++;
+        }
+        return ans;
+    }
+}
+
